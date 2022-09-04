f160328265b543695564ea07eed1ec3ce2a56307
Lucas Fonseca <lucasrfon@gmail.com>
2022-09-04 15:04:43 -0300

    ajusta nome de activationSchema para cardAtivationSchema

--- src/routers/cardRouter.ts
+++ src/routers/cardRouter.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { requestCardCreation } from "../controllers/cardController";
 import validateSchema from "../middlewares/validateSchema";
+import cardActivationSchema from "../schemas/cardActivationSchema";
 import newCardSchema from "../schemas/newCardSchema";
 
 const cardRouter = Router();
 
 cardRouter.post('/card', validateSchema(newCardSchema), requestCardCreation); //falta API e cryptr
-cardRouter.patch('/activate');
+cardRouter.patch('/activate', validateSchema(cardActivationSchema));
 // cardRouter.get('/card');
 cardRouter.patch('/block')
 
--- src/schemas/cardActivationSchema.ts
+++ src/schemas/cardActivationSchema.ts
@@ -1,8 +1,8 @@
 import joi from 'joi';
 
-const newCardSchema = joi.object({
+const cardActivationSchema = joi.object({
     "cvv": joi.string().pattern(/^[0-9]{3}$/).required(),
     "senha": joi.string().required()
 });
 
-export default newCardSchema;
+export default cardActivationSchema;
