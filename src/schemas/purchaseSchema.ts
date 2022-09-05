3a7d42593e2b53f4825429d45555e8f8a8453b32
Lucas Fonseca <lucasrfon@gmail.com>
2022-09-05 00:14:44 -0300

    implementa puschaseSchema

---
+++ src/schemas/purchaseSchema.ts
@@ -0,0 +1,14 @@
+import joi from 'joi';
+import joiDate from '@joi/date';
+const extendedJoi = joi.extend(joiDate)
+
+const purchaseSchema = joi.object({
+    "password": joi.string().pattern(/^[0-9]{4}$/).required(),
+    "number": joi.string().pattern(/^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$/).required(),
+    "cardholderName": joi.string().pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ ]*$/).required(),
+    "expirationDate": extendedJoi.date().format('MM/YY').required(),
+    "amount": joi.number().integer().greater(0).required(),
+    "business": joi.string().required()
+});
+
+export default purchaseSchema;
--- src/schemas/rechargeSchema.ts
+++ src/schemas/rechargeSchema.ts
@@ -3,7 +3,7 @@ import joi from 'joi';
 const rechargeSchema = joi.object({
     "employeeId": joi.number().integer().required(),
     "type": joi.string().valid('groceries', 'restaurant', 'transport', 'education', 'health').required(),
-    "charge": joi.number().integer().greater(0).required()
+    "amount": joi.number().integer().greater(0).required()
 });
 
 export default rechargeSchema;
